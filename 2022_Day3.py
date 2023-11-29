0170451ec37ec529ea9df646dde4d15d9a6ce37b
khicklin23 <99102590+khicklin23@users.noreply.github.com>
2023-11-29 17:27:41 -0600

    Update 2022_Day3.py

--- 2022_Day3.py
+++ 2022_Day3.py
@@ -1,25 +1,27 @@
 fpath = r'C:\Users\hickl\OneDrive\Documents\AdventOfCode\input.txt'
 sum = 0
-#I believe python has a built in function for 
-def find_common(first: str, second: str):
+#Function to check for common char and find numerical value then append it to sum
+def find_common(first: str, second: str , third: str):
     global sum
     for i in first:
         if i in second:
-            if i.isupper():
-                #Return 27-52 for uppercase
-                sum += (ord(i) - ord('A') + 27)
-            else:
-                #Returns 1-26 for lowercase
-                sum += (ord(i) - ord('a') + 1)
-            break
-
+            if i in third:    
+                if i.isupper():
+                    #Return 27-52 for uppercase
+                    sum += (ord(i) - ord('A') + 27)
+                else:
+                    #Returns 1-26 for lowercase
+                    sum += (ord(i) - ord('a') + 1)
+                break
 with open(fpath, 'r') as file:
+    str1,str2,str3="","",""
     for line_number, line in enumerate(file, start=1):
-        line_length = len(line)
-        #Splits str into two even strings
-        first_rucksack = line[:line_length // 2]
-        second_rucksack = line[line_length // 2:]
-        #Plugs each str into function
-        find_common(first_rucksack , second_rucksack)
+        #Assigns line 1,2,3 to variables then calls the function every 3 lines and resets
+        if line_number % 3 == 1:
+            str1 = line.strip()
+        elif line_number % 3 == 2:
+            str2 = line.strip()
+        elif line_number % 3 == 0:
+            str3 = line.strip()
+            find_common(str1 ,str2 ,str3)
 print(sum)
-
