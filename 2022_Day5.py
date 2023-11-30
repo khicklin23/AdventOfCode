4a7b23ad6ae5671dc08fe19e4ef6a76502704e7b
khicklin23 <99102590+khicklin23@users.noreply.github.com>
2023-11-30 15:26:57 -0600

    Update 2022_Day5.py

--- 2022_Day5.py
+++ 2022_Day5.py
@@ -49,3 +49,48 @@ with open(fpath, 'r') as file:
 
 print("Key: ",crates.pop(1), crates.pop(2), crates.pop(3), crates.pop(4), crates.pop(5), crates.pop(6), crates.pop(7), crates.pop(8), crates.pop(9), sep="")
 '''
+#Part Two (Not Working dont know why works on the example they give but not on my input still troubleshooting)
+fpath = r'C:\Users\hickl\OneDrive\Documents\AdventOfCode\input.txt'
+import re #This import is a cheatcode for this stuff
+class Crates:
+    #Standard multi-stack class
+    def __init__(self): 
+        self.stacks = [["W","M","L","F"], ["B", "Z", "B","M","F"], ["H","V","R","S","L","Q"],["F","S","V","Q","P","M","T","J"],["L","S","W"],
+                       ["F","V","P","M","R","J","W"],["J","Q","C","P","N","R","F"],["V","H","P","S","Z","W","R","V"],["B","M","J","C","G","H","Z","W"],[]]
+    def push(self, stack_num, item):
+        self.stacks[stack_num - 1].append(item)
+    
+    def pop(self, stack_num):
+        return self.stacks[stack_num - 1].pop()
+
+    def move(self, from_stack, to_stack):
+        item = self.pop(from_stack)
+        self.push(to_stack, item)
+    def display(self):
+        for i, stack in enumerate(self.stacks, start=1):
+            print(f"Stack {i}: {stack}")
+
+
+crates = Crates()
+
+with open(fpath, 'r') as file:
+    for LineNum, line in enumerate(file, start=1):
+        #Skips first 11 lines but if you wanted to get fancier you could
+        if LineNum < 11:
+            continue
+        myStr = line.strip()
+        #Life Saver
+        pattern = r"move (\d+) from (\d+) to (\d+)"
+        match = re.match(pattern, myStr)
+        amount = int(match.group(1))
+        fromCrate = int(match.group(2))
+        toCrate = int(match.group(3))
+        moveback = amount
+        while amount >0:
+            crates.move(fromCrate, 10)
+            amount -= 1
+        while moveback >0:
+            crates.move(10, toCrate)
+            moveback -= 1
+print(crates.display())
+print("Key: ",crates.pop(1), crates.pop(2), crates.pop(3), crates.pop(4), crates.pop(5), crates.pop(6), crates.pop(7), crates.pop(8), crates.pop(9), sep="")
