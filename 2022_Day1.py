4bd8e6136a8aea9e67d61cfd72baf651a0c50fea
khicklin23 <99102590+khicklin23@users.noreply.github.com>
2023-11-30 00:50:26 -0600

    Update 2022_Day1.py

--- 2022_Day1.py
+++ 2022_Day1.py
@@ -1,28 +1,23 @@
 fpath = r'C:\Users\hickl\OneDrive\Documents\AdventOfCode\input.txt'
-elf,curCal,mostCal1,mostCal2,mostCal3,lineNum= 0 , 0 , 0 , 0 , 0 , 0
+elf,curCal,mostCal1,mostCal2,mostCal3= 0 , 0 , 0 , 0 , 0 , 0
 with open(fpath, 'r') as file:
-
     for line in file:
-        lineNum+=1
-        #If Line Empty / End of elf's calorie list then determine whether or not to store value
+        #Runs when an empty line is met to signify the end of the elf calorie list
         if not line.strip():
+            #Checks to see if it belongs on the podium
             if curCal > mostCal3:
-                #If num1
+                #Determines where (1,2, or 3)
                 if (curCal > mostCal1) & (curCal > mostCal2):
                     mostCal2, mostCal3 = mostCal1, mostCal2
                     mostCal1 = curCal
-                #If num2
                 elif (curCal < mostCal1) & (curCal > mostCal2):
                     mostCal3 ,mostCal2, = mostCal2, curCal
-                #If num3
                 elif (curCal < mostCal1) & (curCal<mostCal2):
                     mostCal3, curCal = curCal, 0
             curCal = 0
             continue
-
-        #If line has int then add to currentCalories
+        #Add line int to currentCalories
         curCal += int(line.strip())
-        print(int(line.strip()))
 
 sumE = (mostCal1 + mostCal2 + mostCal3)
 print("\nThe Sum For Each Of The Individual Of The Top Three Elves Were: ",mostCal1, mostCal2, mostCal3)
