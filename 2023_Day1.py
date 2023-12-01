ed06a6facffa097dcc14c47cbdc40d8e6c7311bd
khicklin23 <99102590+khicklin23@users.noreply.github.com>
2023-12-01 17:01:41 -0600

    Update 2023_Day1.py

--- 2023_Day1.py
+++ 2023_Day1.py
@@ -1,1 +1,14 @@
-print("Hello World")
+#Part One
+"""fpath = r'C:\Users\hickl\OneDrive\Documents\AdventOfCode\input.txt'
+sum = 0
+with open(fpath, 'r') as file:
+    for LineNum, line in enumerate(file, start=1):
+        myStr = line.strip()
+        intArr = []
+        for i in myStr:
+            if i.isdigit():
+                intArr.append(i)
+        var = (str(intArr[0])+str(intArr[-1]))
+        sum += int(var)  
+print(sum)              
+"""
