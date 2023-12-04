899d6e2f1ad2483f7aa66011fc395465a5292fa3
khicklin23 <99102590+khicklin23@users.noreply.github.com>
2023-12-04 16:20:22 -0600

    Update 2023_Day2.py

--- 2023_Day2.py
+++ 2023_Day2.py
@@ -1,32 +1,46 @@
-file_path_MacBook = r'/Users/kadenhicklin/Desktop/AdventOfCode/input.txt'
-file_path_PC = r'C:\Users\hickl\OneDrive\Documents\AdventOfCode\input.txt'
 import re
 
 def partOne(file_input: str):
     sum = 0
     with open(file_input, 'r') as file:
         redMax, greenMax, blueMax = 12,13,14
+        key = 0
         for LineNum, line in enumerate(file, start=1):
             myStr = line.strip()
-            myStr.replace(',','')
-            print(myStr)
+            posGame = True
             strArr = myStr.split(' ')
+            #Remove "Game N:"
             strArr = strArr[2:]
-            for i in strArr:
-                if i%2 == 0:
-                    if "blue" in strArr[i]:
-                        b += d
-                r,g,b = 0,0,0
-            print(strArr)
-            
-            
-            
-            
-"""def partTwo(file_input: str):
+            for index, i in enumerate(strArr):
+                #Every odd index of strArr is a color name and index prior is the amount
+                if index % 2 != 0:
+                    if "blue" in i and int(strArr[index-1]) > blueMax:
+                        posGame = False
+                    elif "red" in i and int(strArr[index-1]) > redMax:
+                        posGame = False
+                    elif "green" in i and int(strArr[index-1]) > greenMax:
+                        posGame = False
+            if posGame == True:
+                #Add Game ID to running count
+                key += LineNum
+        print(key)
+        
+def partTwo(file_input: str):
     sum = 0
     with open(file_path, 'r') as file:
-        for LineNum, line in enumerate(file, start=1):"""
-            
-partOne(file_path_PC)
-#partTwo(file_path_PC)
+        for LineNum, line in enumerate(file, start=1):                
+            #In progress
+    
+
+
+
+#PC
+partOne(r'C:\Users\hickl\OneDrive\Documents\AdventOfCode\input.txt')
+#partTwo(r'C:\Users\hickl\OneDrive\Documents\AdventOfCode\input.txt')
+
+#MacBook
+#partOne(r'/Users/kadenhicklin/Desktop/AdventOfCode/input.txt')
+#partTwo(r'/Users/kadenhicklin/Desktop/AdventOfCode/input.txt')
+
+
 
