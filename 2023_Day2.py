62c4187fc319ecd5d9e3d0186822ded3e0192326
khicklin23 <99102590+khicklin23@users.noreply.github.com>
2023-12-04 15:39:14 -0600

    Update 2023_Day2.py

--- 2023_Day2.py
+++ 2023_Day2.py
@@ -1,1 +1,33 @@
+file_path = r'/Users/kadenhicklin/Desktop/AdventOfCode/input.txt'
+import re
+
+
+def partOne(file_input: str):
+    sum = 0
+    with open(file_input, 'r') as file:
+        redMax, greenMax, blueMax = 12,13,14
+        for LineNum, line in enumerate(file, start=1):
+            myStr = line.strip()
+            myStr.replace(',','')
+            print(myStr)
+            strArr = myStr.split(' ')
+            strArr = strArr[2:]
+            for i in strArr:
+                if i%2 == 0:
+                    if "blue" in strArr[i]:
+                        b += d
+                r,g,b = 0,0,0
+            print(strArr)
+            
+            
+            
+            
+"""def partTwo(file_input: str):
+    sum = 0
+    with open(file_path, 'r') as file:
+        for LineNum, line in enumerate(file, start=1):"""
+            
+partOne('/Users/kadenhicklin/Desktop/AdventOfCode/input.txt')
+#partTwo('/Users/kadenhicklin/Desktop/AdventOfCode/input.txt')
+
 
