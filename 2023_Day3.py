e1404cc7835bc4ad46c218d8f8edb7044c4311c3
khicklin23 <99102590+khicklin23@users.noreply.github.com>
2023-12-05 20:51:22 -0600

    Update 2023_Day3.py

--- 2023_Day3.py
+++ 2023_Day3.py
@@ -1,44 +1,136 @@
+#2023 Day 3
+#I don't wanna talk about how long this took me
+
+def integrityCheck(matrix: list):
+    for Row, currentLine in enumerate(matrix):
+        for i in currentLine:
+            #Doesn't recognize the numbers (T) as foreign chars
+            if i != '.' and i != 'T':
+                return True
+
 
 
 def partOne(file_input: str):
     sum = 0
-    twoDarr = []
+    matrix = []
     with open(file_input, 'r') as file:
         for line in file:
-            row = list(line.strip())
-            twoDarr.append(row)
-    for row in twoDarr:
-        for i in row:
-            if i.isdigit() and [i + 1].isdigit() and [i + 2].isdigit():
-                #Something like this to check for three digit then check surroundings but out of index errors might be a pain idk gtg tho
-                
-        break
+            currentLine = list(line.strip())
+            matrix.append(currentLine)
+        Column = 0
+    for Row, currentLine in enumerate(matrix):
+        while Column < len(currentLine):
+
+            #Check For Three Digit Num First (Important)
+            if Column+2 < len(currentLine) and currentLine[Column].isdigit() and currentLine[Column+1].isdigit() and currentLine[Column+2].isdigit():
+                #Grab value of the 3 digits because were going to be changing that later
+                num = int(matrix[Row][Column]+matrix[Row][Column+1]+matrix[Row][Column+2])
+                #Set the baseline for the surrounding digit matrix dimensions
+                minH,maxH, minW, maxW = -1,2,-1,4
+
+                #Change the number to something else to make parsing through the matrix earlier
+                #The matrixes will be different sizes so the integrityCheck() needs to know where the number location is
+                matrix[Row][Column] = 'T'
+                matrix[Row][Column+1] = 'T'
+                matrix[Row][Column+2] = 'T'
+
+                #Alters the perameters for the matrix if you are close to an edge
+                if Row == 0:
+                    minH = 0
+                if Row == 139:
+                    maxH = 0
+                if Column == 0:
+                    minW = 0
+                if Column == len(currentLine)-3:
+                    maxW = 3
+
+                #Grabs the rows above and below the numbers (minH and maxH will change the rows grabbed if needed)
+                testRows = matrix[Row+minH:Row+maxH]
+                #Creates an array of all surrounding chars (minW and maxW will change the columns grabbed if needed)
+                #Defintely didn't use google for this little function lol but I did alter it with the minW and stuff
+                surroundings = [row[Column+minW:Column+maxW] for row in testRows]
+
+                #Calls the integrity check to see if it can find any non '.' chars surrounding the letters
+                if integrityCheck(surroundings) == True:
+                    sum += int(num)
+
+                #Skip ahead 3 spaces so that the following nums arent re-read as 2 or 1 digit nums
+                Column += 3
+
+            #Two Digit Num (Same but with different values)
+            elif Column+1 < len(currentLine) and currentLine[Column].isdigit() and currentLine[Column+1].isdigit():
+                num = int(matrix[Row][Column]+matrix[Row][Column+1])
+                minH,maxH, minW, maxW = -1,2,-1,3
+                matrix[Row][Column] = 'T'
+                matrix[Row][Column+1] = 'T'
+                if Row == 0:
+                    minH = 0
+                if Row == 139:
+                    maxH = 0
+                if Column == 0:
+                    minW = 0
+                if Column == len(currentLine)-2:
+                    maxW = 2 
+
+                testRows = matrix[Row+minH:Row+maxH]
+                surroundings = [row[Column+minW:Column+maxW] for row in testRows]
+                if integrityCheck(surroundings) == True:
+                    sum += int(num)
+                Column += 2
+            
+            #One Digit Num (Same but with different values)
+            elif Column < len(currentLine) and currentLine[Column].isdigit():
+                num = int(matrix[Row][Column])
+                minH,maxH, minW, maxW = -1,2,-1,2
+                matrix[Row][Column] = 'T'
+                if Row == 0:
+                    minH = 0
+                if Row == 139:
+                    maxH = 0
+                if Column == 0:
+                    minW = 0
+                if Column == len(currentLine)-1:
+                    maxW = 0
+
+                testRows = matrix[Row+minH:Row+maxH]
+                surroundings = [row[Column+minW:Column+maxW] for row in testRows]
+                if integrityCheck(surroundings) == True:
+                    sum += int(num)
+                Column += 2
+
+            else:
+                #If not a num then continue
+                Column +=1
+        Column = 0
+    print(sum)
 
 
-"""
-Pseudocode
-    2d array
 
-    Algorithim for single digits:
-    if (i, row +- 1) != . -check above and below
-       (i+-1 ,row +- 1) . -check perpendicular
-    
-    -Build string of chars then once next char down is != num then check but will have to account for length of num 
-    -I think I'm going to cheat and check and see if its a one digit, two digit, or three digit num
-        this will make it a lot easier than enabling ulimited digits
+def partTwo(file_input: str):
+    sum = 0
+    matrix = []
+    with open(file_input, 'r') as file:
+        for line in file:
+            currentLine = list(line.strip())
+            matrix.append(currentLine)
+        Column = 0
+    for Row, currentLine in enumerate(matrix):
+        while Column < len(currentLine):
+            if matrix[Row][Column] == '*':
+                print()
 
-"""
-
+   
 
 #PC
 print("Part One Answer:")
 partOne(r'C:\Users\hickl\OneDrive\Documents\AdventOfCode\input.txt')
+print("Part Two Answer:")
+partTwo(r'C:\Users\hickl\OneDrive\Documents\AdventOfCode\input.txt')
 
 
 #MacBook
 #partOne(r'/Users/kadenhicklin/Desktop/AdventOfCode/input.txt')
 #partTwo(r'/Users/kadenhicklin/Desktop/AdventOfCode/input.txt')
-
 
 
 
